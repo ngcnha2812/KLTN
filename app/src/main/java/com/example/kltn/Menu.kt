e0521a5b22c7d8d76b8e0784fb5bffee531b0dbd
Ngọc Nhã <ngcnha2812@gmail.com>
2022-03-19 16:18:46 +0700

    Sua giao dien Fan

---
+++ app/src/main/java/com/example/kltn/Menu.kt
@@ -0,0 +1,22 @@
+package com.example.kltn
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        val fan:ImageView = findViewById(R.id.fan)
+        val tv:ImageView = findViewById(R.id.tv)
+        val air:ImageView = findViewById(R.id.air)
+
+        val MeFan = Intent(this,FanAct::class.java)
+        fan.setOnClickListener {
+            startActivity(MeFan)
+        }
+    }
+
+}
--- app/src/main/java/com/example/kltn/Splash.kt
+++ app/src/main/java/com/example/kltn/Splash.kt
@@ -1,11 +1,27 @@
 package com.example.kltn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import android.os.Looper
+import kotlin.concurrent.timer
 
 class Splash : AppCompatActivity() {
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.`splash`)
+        val menu = Intent(this,Menu::class.java)
+        handler = Handler(Looper.getMainLooper());
+        val runnable: Runnable = object :Runnable {
+            override fun run() {
+                finish()
+                startActivity(menu)
+            }
+        }
+        handler.postDelayed(runnable,5000);
+
     }
 }
