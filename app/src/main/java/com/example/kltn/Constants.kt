1d953f242a22f1c7d7793868026aaeb5ebc079f2
NVT101100 <nguyenvanthoi.1011.bd@gmail.com>
2022-03-23 09:29:47 +0700

    MQTT connect

---
+++ app/src/main/java/com/example/kltn/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.kltn
+
+object Constants {
+    const val databaseURL : String = "https://kltn-344900-default-rtdb.asia-southeast1.firebasedatabase.app"
+}
--- app/src/main/java/com/example/kltn/FanAct.kt
+++ app/src/main/java/com/example/kltn/FanAct.kt
@@ -2,11 +2,24 @@ package com.example.kltn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import io.grpc.internal.JsonParser
 
 class FanAct : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
     private lateinit var client  : MqttConnect
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fan)
+        database = Firebase.database(Constants.databaseURL).reference
+        database.child("Fan").child("Samsung").get().addOnSuccessListener {
+           Log.d("firebase","${it.value}")
+        }.addOnFailureListener {
+            Log.d("error","Error occured")
+        }
+
     }
 }
