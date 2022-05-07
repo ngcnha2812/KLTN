e46c6811e79414d1b00039f9debad4389e764ef7
NVT101100 <nguyenvanthoi.1011.bd@gmail.com>
2022-05-07 16:47:44 +0700

    test TV

--- app/src/main/java/com/example/kltn/Button_TV_Act.kt
+++ app/src/main/java/com/example/kltn/Button_TV_Act.kt
@@ -96,17 +96,17 @@ class Button_TV_Act : AppCompatActivity() {
         }
         ttv.setOnClickListener {
             dataBase.child("$name/ttv").get().addOnSuccessListener {
-                mqttClient.Send("Customs","${it.value.toString()}/$protocol/$bitnum")
+                mqttClient.Send("TV","${it.value.toString()}/$protocol/$bitnum")
             }
         }
         voldown.setOnClickListener {
             dataBase.child("$name/Voldown").get().addOnSuccessListener {
-                mqttClient.Send("Customs","${it.value.toString()}/$protocol/$bitnum")
+                mqttClient.Send("TV","${it.value.toString()}/$protocol/$bitnum")
             }
         }
         volup.setOnClickListener {
             dataBase.child("$name/Volup").get().addOnSuccessListener {
-                mqttClient.Send("Customs","${it.value.toString()}/$protocol/$bitnum")
+                mqttClient.Send("TV","${it.value.toString()}/$protocol/$bitnum")
             }
         }
     }
